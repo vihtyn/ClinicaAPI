b30c70c410f840a04414ae6633f8cac4a4b782c1
renanzo2 <renanwatanabe802@gmail.com>
2025-11-12 16:34:29 -0300

    ADD ON DELETE CASCADE para as tabelas consulta e tratamento

--- App_clinica_vet/updates.sql
+++ App_clinica_vet/updates.sql
@@ -6,3 +6,11 @@ ADD COLUMN hora_consulta TIME;
 
 ALTER TABLE 
 consulta ADD COLUMN tipo VARCHAR(50);
+
+ALTER TABLE tratamento
+DROP FOREIGN KEY FK_id_consulta;
+
+ALTER TABLE tratamento
+ADD CONSTRAINT FK_id_consulta
+	FOREIGN KEY (id_consulta) REFERENCES consulta(id)
+    ON DELETE CASCADE;
